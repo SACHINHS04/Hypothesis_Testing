402a58866921f8cecafbbc3c3685a08b175411f9
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-02-10 16:13:31 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,71 +1,62 @@
-import pandas as pd
-import numpy as np
-import scipy.stats as stats
 import streamlit as st
+import pandas as pd
+import scipy.stats as stats
 
 @st.cache_data
-def load_data(file=None):
+def load_data(file):
     if file is None:
         return None
     df = pd.read_csv(file)
     return df
 
 def t_test(df, column1, column2, alpha):
-    result = stats.ttest_ind(df[column1], df[column2])
-    t_statistic = result[0]
-    p_value = result[1]
-    if p_value < alpha:
-        st.write(f"With a significance level of {alpha}, the two-sample t-test is significant.")
-        st.write(f"The t-statistic is {t_statistic:.4f} and the p-value is {p_value:.4f}.")
+    t, p = stats.ttest_ind(df[column1], df[column2])
+    if p < alpha:
+        st.write(f"With a p-value of {p:.4f}, we reject the null hypothesis that the means of {column1} and {column2} are equal.")
     else:
-        st.write(f"With a significance level of {alpha}, the two-sample t-test is not significant.")
-        st.write(f"The t-statistic is {t_statistic:.4f} and the p-value is {p_value:.4f}.")
+        st.write(f"With a p-value of {p:.4f}, we fail to reject the null hypothesis that the means of {column1} and {column2} are equal.")
 
-def anova(df, column, group_column, alpha):
-    result = stats.f_oneway(df[df[group_column] == group][column] for group in df[group_column].unique())
-    f_statistic = result[0]
-    p_value = result[1]
-    if p_value < alpha:
-        st.write(f"With a significance level of {alpha}, the ANOVA test is significant.")
-        st.write(f"The F-statistic is {f_statistic:.4f} and the p-value is {p_value:.4f}.")
+def anova(df, column1, group_column, alpha):
+    groups = df.groupby(group_column)
+    f, p = stats.f_oneway(*[group[1][column1] for group in groups])
+    if p < alpha:
+        st.write(f"With a p-value of {p:.4f}, we reject the null hypothesis that the means of {column1} are equal among the different groups defined by {group_column}.")
     else:
-        st.write(f"With a significance level of {alpha}, the ANOVA test is not significant.")
-        st.write(f"The F-statistic is {f_statistic:.4f} and the p-value is {p_value:.4f}.")
+        st.write(f"With a p-value of {p:.4f}, we fail to reject the null hypothesis that the means of {column1} are equal among the different groups defined by {group_column}.")
 
 def chi_squared_test(df, column1, column2, alpha):
-    result = stats.chi2_contingency(pd.crosstab(df[column1], df[column2]))
-    chi2_statistic = result[0]
-    p_value = result[1]
-    if p_value < alpha:
-        st.write(f"With a significance level of {alpha}, the Chi-Squared test is significant.")
-        st.write(f"The Chi-Squared statistic is {chi2_statistic:.4f} and the p-value is {p_value:.4f}.")
+    contingency_table = pd.crosstab(df[column1], df[column2])
+    chi2, p, dof, expected = stats.chi2_contingency(contingency_table)
+    if p < alpha:
+        st.write(f"With a p-value of {p:.4f}, we reject the null hypothesis that {column1} and {column2} are independent.")
     else:
-        st.write(f"With a significance level of {alpha}, the Chi-Squared test is not significant.")
-        st.write(f"The Chi-Squared statistic is {chi2_statistic:.4f} and the p-value is {p_value:.4f}.")
+        st.write(f"With a p-value of {p:.4f}, we fail to reject the null hypothesis that {column1} and {column2} are independent.")
 
 def main():
-    st.write("Upload your data file")
-    file = st.file_uploader("Choose a CSV file", type="csv")
-    df= load_data(file)
+    st.write("Upload a file to perform hypothesis testing:")
+    file = st.file_uploader("", type=["csv"])
+    df = load_data(file)
+
     if df is None:
         st.write("No data was uploaded.")
-    return
+        return
 
-st.write("Select the test you want to perform:")
-test = st.selectbox("Test", ["T-Test", "ANOVA", "Chi-Squared Test"])
-st.write("Select the columns you want to use:")
-column1 = st.selectbox("Column 1", df.columns)
-if test == "T-Test":
-    column2 = st.selectbox("Column 2", df.columns)
-    alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
-    t_test(df, column1, column2, alpha)
-elif test == "ANOVA":
-    group_column = st.selectbox("Grouping Column", df.columns)
-    alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
-    anova(df, column1, group_column, alpha)
-elif test == "Chi-Squared Test":
-    column2 = st.selectbox("Column 2", df.columns)
-    alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
-    chi_squared_test(df, column1, column2, alpha)
-if name == 'main':
+    st.write("Select the test you want to perform:")
+    test = st.selectbox("Test", ["T-Test", "ANOVA", "Chi-Squared Test"])
+    st.write("Select the columns you want to use:")
+    column1 = st.selectbox("Column 1", df.columns)
+    if test == "T-Test":
+        column2 = st.selectbox("Column 2", df.columns)
+        alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
+                t_test(df, column1, column2, alpha)
+    elif test == "ANOVA":
+        group_column = st.selectbox("Grouping Column", df.columns)
+        alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
+        anova(df, column1, group_column, alpha)
+    elif test == "Chi-Squared Test":
+        column2 = st.selectbox("Column 2", df.columns)
+        alpha = st.slider("Significance level (alpha)", 0.0, 1.0, step=0.01, value=0.05)
+        chi_squared_test(df, column1, column2, alpha)
+
+if __name__ == "__main__":
     main()
